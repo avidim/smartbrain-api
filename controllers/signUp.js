77ab3b3089349e0ec5e84695cebe700349fb6046
Javid <javidgasimzada@gmail.com>
2020-08-03 16:34:30 +0400

    upd dt 2

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -19,7 +19,7 @@ const handler = (req, res, db, bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback)
     })
-        .catch(err => res.status(400).send('error'))
+        .catch(err => res.status(400).send(err))
 };
 
 module.exports = {
